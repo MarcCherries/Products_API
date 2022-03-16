c9c7aee73c28b904ae5d63e51637f14c730b50fa
Chris Creamer <chrisc1983@gmail.com>
2022-03-16 13:12:38 -0700

    finished the last of the views functions, tested all endpoints in postman to success

--- Products/models.py
+++ Products/models.py
@@ -3,6 +3,6 @@ from django.db import models
 # Create your models here.
 class Product(models.Model):
     title = models.CharField(max_length=255)
-    descrption = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
     price = models.DecimalField(max_digits=8, decimal_places=2)
     inventory_quantity = models.IntegerField()
--- Products/views.py
+++ Products/views.py
@@ -1,12 +1,37 @@
+from urllib import response
 from django.shortcuts import render
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from .serializers import ProductSerializer
 from .models import Product
+from rest_framework import status
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
-@api_view(['GET'])
-def get_products_list(request):
-    products = Product.objects.all()
-    serializer = ProductSerializer(products, many=True)
-    return Response(serializer.data)
+@api_view(['GET', 'POST'])
+def products_list(request):
+    if request.method == 'GET':
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def product_detail(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        serializer = ProductSerializer(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'PUT':
+        serializer = ProductSerializer(product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == 'DELETE':
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
