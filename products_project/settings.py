79dce98143219577b3c3179f67838ed136df6f96
Chris Creamer <chrisc1983@gmail.com>
2022-03-17 07:18:57 -0700

    final commit with bonus user story and addition of coerce_decimal_to_strinng in settings file

--- products_project/settings.py
+++ products_project/settings.py
@@ -13,6 +13,9 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 from tracemalloc import reset_peak
 
+Rest_Framework = {
+    'COERCE_DECIMAL_TO_STRING':False
+}
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
